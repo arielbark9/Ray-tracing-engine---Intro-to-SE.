6a4fe250cc894119976c71c9af580620470e617c
ariel <arielbark9@gmail.com>
2021-04-21 20:22:53 +0300

    Triangle intersections

--- Tests/unittests/geometries/TriangleTest.java
+++ Tests/unittests/geometries/TriangleTest.java
@@ -4,7 +4,10 @@ import geometries.Triangle;
 import org.junit.Before;
 import org.junit.Test;
 import primitives.Point3D;
+import primitives.Ray;
 import primitives.Vector;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -17,4 +20,49 @@ public class TriangleTest {
         assertEquals("Error: getNormal() does not return the right vector!",
                 new Vector(0,0,1), tr.getNormal(new Point3D(1,0,0)));
     }
+
+    /**
+     * Test method for {@link geometries.Triangle#findIntersections(primitives.Ray)}.
+     */
+    @Test
+    public void testFindIntersections() {
+        Triangle triangle = new Triangle(new Point3D(2, 0, 0),new Point3D(-2, 0, 0),new Point3D(0, 0, 2));
+
+        // ============ Equivalence Partitions Tests ==============
+
+        // TC01: Point is in triangle (1 point)
+        Point3D p1 = new Point3D(0,0,1);
+        List<Point3D> res = triangle.findIntersections(new Ray(new Point3D(0,-1,1),
+                new Vector(0,1,0)));
+        assertEquals("Wrong number of points", 1, res.size());
+        assertEquals("Wrong intersection point", p1, res.get(0));
+
+        // TC02: Point is outside against the edge of the triangle (0 points)
+        res = triangle.findIntersections(new Ray(new Point3D(2,-1,2),
+                new Vector(0,1,0)));
+        assertNull("Wrong number of points",res);
+
+        // TC03: Point is outside against the vertex of the triangle (0 points)
+        res = triangle.findIntersections(new Ray(new Point3D(0,-1,3),
+                new Vector(0,1,0)));
+        assertNull("Wrong number of points",res);
+
+        // =============== Boundary Values Tests ==================
+
+        // TC11: Point is on an edge (0 points)
+        res = triangle.findIntersections(new Ray(new Point3D(1,-1,1),
+                new Vector(0,1,0)));
+        assertNull("Wrong number of points",res);
+
+        // TC12: Point is on a vertex (0 points)
+        res = triangle.findIntersections(new Ray(new Point3D(2,-1,0),
+                new Vector(0,1,0)));
+        assertNull("Wrong number of points",res);
+
+        // TC13: Point is on a continuation of an edge (0 points)
+        res = triangle.findIntersections(new Ray(new Point3D(1,-1,3),
+                new Vector(0,1,0)));
+        assertNull("Wrong number of points",res);
+    }
+
 }
--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -1,6 +1,9 @@
 package geometries;
 import primitives.*;
 
+import java.util.List;
+import static primitives.Util.*;
+
 /**
  * Triangle class represents two-dimensional triangle in 3D Cartesian coordinate
  * system
@@ -22,37 +25,38 @@ public class Triangle extends Polygon{
                 vertices.get(1).toString() + ',' +
                 vertices.get(2).toString();
     }
-}
 
+    @Override
+    public List<Point3D> findIntersections(Ray ray) {
+        List<Point3D> points = plane.findIntersections(ray);
+        Point3D p0 = points.get(0);
 
+        Vector v1,v2,v3;
+        try {
+            v1 = vertices.get(0).subtract(p0); // v1 = p1-p0
+            v2 = vertices.get(1).subtract(p0); // v2 = p2-p0
+            v3 = vertices.get(2).subtract(p0); // v3 = p3-p0
+        } catch (IllegalArgumentException ex) { // point on vertex because vector 0.
+            return null;
+        }
 
+        Vector n1,n2,n3;
+        try {
+            n1 = v1.crossProduct(v2); // n1 = v1 x v2
+            n2 = v2.crossProduct(v3); // n2 = v2 x v3
+            n3 = v3.crossProduct(v1); // n3 = v3 x v1
+        } catch (IllegalArgumentException ex) { // point on edge because vector 0.
+            return null;
+        }
 
+        double vn1 = alignZero(ray.getDir().dotProduct(n1));
+        double vn2 = alignZero(ray.getDir().dotProduct(n2));
+        double vn3 = alignZero(ray.getDir().dotProduct(n3));
 
-
-
-
-
-
-
-
-
- /*boolean CheckPointInTriangle(Point3D P)//function that checks if point is in the triangle
-    {
-
-        Vector v0 = vertices.get(2).subtract(vertices.get(0));
-        Vector v1 = vertices.get(1).subtract(vertices.get(0));
-        Vector v2 = P.subtract(vertices.get(0));
-
-        double dot00 = v0.dotProduct(v0);
-        double dot01 = v0.dotProduct(v1);
-        double dot02 = v0.dotProduct(v2);
-        double dot11 = v1.dotProduct(v1);
-        double dot12 = v1.dotProduct(v2);
-
-       double invDenom = 1 / (dot00 * dot11 - dot01 * dot01);
-       double u = (dot11 * dot02 - dot01 * dot12) * invDenom;
-       double v = (dot00 * dot12 - dot01 * dot02) * invDenom;
-
-        return (u >= 0) && (v >= 0) && (u + v < 1);
+        if (isZero(vn1) || isZero(vn2) || isZero(vn3)) return null;
+        if ((vn1 > 0 && vn2 > 0 && vn3 > 0) || (vn1 < 0 && vn2 < 0 && vn3 < 0)) {
+            return points;
+        }
+        return null;
     }
-*/
+}
